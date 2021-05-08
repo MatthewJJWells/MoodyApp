7d469b20f8c12da3f0e7ea9275c456b1aebcd561
Marshall <danielm22@hotmail.co.uk>
2021-05-08 17:19:22 +0100

    feat: added Home screen/ requiring through index

---
+++ Screens/Home/Home.screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const Home: React.FC<Record<string, never>> = () => {
+  return (
+    <View>
+      <Text>Hey</Text>
+    </View>
+  );
+};
+export default Home;
