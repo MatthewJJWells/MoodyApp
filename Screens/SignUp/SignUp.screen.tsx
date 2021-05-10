8a31ac33a99fecc99fe7b2ef5711d2b22599910f
Matt <mattywells98@gmail.com>
2021-05-10 14:48:03 +0100

    stack navigation with type errors 2

--- Screens/SignUp/SignUp.screen.tsx
+++ Screens/SignUp/SignUp.screen.tsx
@@ -1,33 +1,47 @@
 import React from 'react';
 import styles from './SignUp.style';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { FormButton } from '../../components';
 
-const SignUp: React.FC<Record<string, never>> = () => {
+const SignUp: React.FC<{}> = ({ navigation }) => {
+  function navigationCreateAccount() {
+    navigation.navigate('Home');
+  }
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={{fontSize: 40}}>Moody App</Text>
+        <Text style={{ fontSize: 40 }}>Moody App</Text>
       </View>
       <View style={styles.footer}>
         <Text>Sign Up Screen</Text>
         <View style={styles.inputContainer}>
-          <TextInput style={styles.textInputStyle} placeholder='Email'></TextInput>
-        </View >
-        <View style={styles.inputContainer}>
-          <TextInput style={styles.textInputStyle} placeholder='Username'></TextInput>
-        </View >
-        <View style={styles.inputContainer}>
-          <TextInput style={styles.textInputStyle} placeholder='Phone'></TextInput>
+          <TextInput
+            style={styles.textInputStyle}
+            placeholder="Email"
+          ></TextInput>
         </View>
         <View style={styles.inputContainer}>
-          <TextInput style={styles.textInputStyle} placeholder='Password'></TextInput>
+          <TextInput
+            style={styles.textInputStyle}
+            placeholder="Username"
+          ></TextInput>
         </View>
-        <TouchableOpacity style={styles.buttonStyle}>
-          <Text style={styles.buttonText}>Log In</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.buttonStyle}>
-          <Text style={styles.buttonText}>Create Account</Text>
-        </TouchableOpacity>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInputStyle}
+            placeholder="Phone"
+          ></TextInput>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInputStyle}
+            placeholder="Password"
+          ></TextInput>
+        </View>
+        <FormButton
+          buttonName="Create Account"
+          onPress={navigationCreateAccount}
+        ></FormButton>
       </View>
     </View>
   );
