95e7c0605219a2259f6ddf59b78a50a3bda7263f
Matt <mattywells98@gmail.com>
2021-05-10 09:29:48 +0100

    start of button

--- components/FormButton.screen.tsx
+++ components/FormButton.screen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Touchable, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import styles from './FormButton.styles';
 
 const FormButton: React.FC<Record<string, never>> = () => {
