079900e3ac9c8648e58b324795965b12fbd5ab74
Matt <mattywells98@gmail.com>
2021-05-10 12:26:05 +0100

    Reducer and store

---
+++ Store/reducer.ts
@@ -0,0 +1,30 @@
+/* eslint-disable indent */
+import { AnyAction } from 'redux';
+
+interface userState {
+  isLoggedIn: boolean;
+  userName: string | null;
+}
+const initialState: userState = {
+  isLoggedIn: false,
+  userName: null,
+};
+
+const reducer = (state = initialState, action: AnyAction): unknown => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        isLoggedIn: true,
+        userName: action.payload,
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        isLoggedIn: false,
+        userName: null,
+      };
+  }
+};
+
+export default reducer;
