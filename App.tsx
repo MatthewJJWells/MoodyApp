7e1de47a94fb9ea326490ee07d76e72b0cae9f3e
Marshall <danielm22@hotmail.co.uk>
2021-05-11 18:03:58 +0100

    chore: deleted un-used imports

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Dashboard, Home, SignUp } from './Screens/index';
-import { StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
