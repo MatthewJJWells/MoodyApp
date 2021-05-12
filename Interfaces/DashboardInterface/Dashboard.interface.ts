c9f6d0c01a54af17d46a52722e6de68875001064
Matt <mattywells98@gmail.com>
2021-05-12 22:03:29 +0100

    AddMood nav

--- Interfaces/DashboardInterface/Dashboard.interface.ts
+++ Interfaces/DashboardInterface/Dashboard.interface.ts
@@ -1,8 +1,20 @@
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../navigationInterfaces';
+
+type DashboardScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Login'
+>;
+
+interface DashboardNavigationProps {
+  navigation: DashboardScreenNavigationProp;
+}
+
 type DashboardProps = {
   isLoggedIn: boolean;
   userName: string;
 } & {
   submit: (userName: string) => void;
-};
+} & DashboardNavigationProps;
 
 export { DashboardProps };
--- Screens/Dashboard/Dashboard.screen.tsx
+++ Screens/Dashboard/Dashboard.screen.tsx
@@ -18,7 +18,10 @@ const moodyPosts = [
   { id: 3, date: '30/004/21', mood_text: 'meh', rating: 5, personal_note: '' },
 ];
 
-const Dashboard: React.FC<DashboardProps> = ({ userName }) => {
+const Dashboard: React.FC<DashboardProps> = ({ navigation, userName }) => {
+  const navigationAddMood = () => {
+    navigation.navigate('AddMood');
+  };
   return (
     <View style={styles.container}>
       <Text>{userName}</Text>
@@ -30,7 +33,7 @@ const Dashboard: React.FC<DashboardProps> = ({ userName }) => {
           renderItem={({ item }) => <MoodPost item={item} />}
         ></FlatList>
       </View>
-      <MoodEntryButton></MoodEntryButton>
+      <MoodEntryButton onPress={navigationAddMood}></MoodEntryButton>
     </View>
   );
 };
