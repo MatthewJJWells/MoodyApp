d3349d450a7feb5ea54e615ebe5d1a5d20659b0c
Anne Berg Jolis <annejolis@gmail.com>
2021-05-13 19:39:00 +0100

    feat: frontend calls api to add moods to dashboard

--- Screens/Dashboard/Dashboard.screen.tsx
+++ Screens/Dashboard/Dashboard.screen.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList } from 'react-native';
 import styles from './DashBoard.style';
 
-import { getUserRecords } from '../../services/ApiService';
+import { addRecord, getUserRecords } from '../../services/ApiService';
 
 import {
   MoodEntryButton,
@@ -14,19 +14,16 @@ import {
 import { loginConnector } from '../../Utilities/Login.utlilities';
 import { DashboardProps } from '../../Interfaces/';
 
-const moodyPosts = [
-  { id: 1, date: '21/03/21', mood_text: 'happy', rating: 8, personal_note: '' },
-  { id: 2, date: '22/04/21', mood_text: 'sad', rating: 2, personal_note: '' },
-  { id: 3, date: '30/004/21', mood_text: 'meh', rating: 5, personal_note: '' },
-];
-
 const Dashboard: React.FC<DashboardProps> = ({ navigation, userName }) => {
-  const [gotRecords] = useState(false);
+  const [moodyPosts, setMoodyPosts] = useState([]);
+  const [gotRecords, setGotRecords] = useState(false);
+
   useEffect(() => {
-    console.log("we're doiing things right");
     if (!gotRecords) {
-      const records = getUserRecords(0);
-      console.log('records:', records);
+      getUserRecords(15).then((records) => {
+        setMoodyPosts(records);
+        setGotRecords(true);
+      });
     }
   });
 
