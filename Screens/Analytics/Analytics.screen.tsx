1a8ea79cfae3d048aba5720a9806681ebc1927da
Anne Berg Jolis <annejolis@gmail.com>
2021-05-17 16:46:55 +0100

    chore: test analytics page with dummy text

--- Screens/Analytics/Analytics.screen.tsx
+++ Screens/Analytics/Analytics.screen.tsx
@@ -4,7 +4,31 @@ import { View, Text } from 'react-native';
 const Analytics: React.FC = () => {
   return (
     <View>
-      <Text>HI</Text>
+      <Text>
+        Cat not kitten around have secret plans yet friends are not food yet
+        miaow then turn around and show you my bum yet make meme, make cute
+        face. Pet me pet me don't pet me groom forever, stretch tongue and leave
+        it slightly out, blep, sugar, my siamese, stalks me (in a good way), day
+        and night . Claws in your leg jump on human and sleep on her all night
+        long be long in the bed, purr in the morning and then give a bite to
+        every human around for not waking up request food, purr loud scratch the
+        walls, the floor, the windows, the humans making bread on the bathrobe i
+        will be pet i will be pet and then i will hiss and stare at imaginary
+        bug. Snob you for another person human is washing you why halp oh the
+        horror flee scratch hiss bite yet proudly present butt to human for
+        touch water with paw then recoil in horror purr or kitty kitty. To pet a
+        cat, rub its belly, endure blood and agony, quietly weep, keep rubbing
+        belly stare out the window floof tum, tickle bum, jellybean footies
+        curly toes, stinky cat purr when give birth. All of a sudden cat goes
+        crazy drink water out of the faucet pee on walls it smells like
+        breakfast run around the house at 4 in the morning for pushes butt to
+        face or lick arm hair. Plan steps for world domination taco cat
+        backwards spells taco cat. I could pee on this if i had the energy going
+        to catch the red dot today going to catch the red dot today really likes
+        hummus, sniff catnip and act crazy or relentlessly pursues moth but
+        spill litter box, scratch at owner, destroy all furniture, especially
+        couch meowwww.
+      </Text>
     </View>
   );
 };
