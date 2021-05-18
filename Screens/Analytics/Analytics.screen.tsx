5feed9a01e0547f835340f80ccc0ebac09269482
Marshall <danielm22@hotmail.co.uk>
2021-05-18 10:30:33 +0100

    pie chart displaying with dummy data

--- Screens/Analytics/Analytics.screen.tsx
+++ Screens/Analytics/Analytics.screen.tsx
@@ -1,11 +1,68 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import styles from './Analytics.style';
+import { PieChart } from 'react-native-chart-kit';
 
 const Analytics: React.FC = () => {
+  const pieData = [
+    {
+      name: 'Seoul',
+      population: 215,
+      color: 'rgba(131, 167, 234, 1)',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Toronto',
+      population: 300,
+      color: '#F00',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Beijing',
+      population: 400,
+      color: '#4da550',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'New York',
+      population: 250,
+      color: '#ffffff',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Moscow',
+      population: 500,
+      color: 'rgb(0, 0, 255)',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>HI</Text>
+      <PieChart
+        data={pieData}
+        width={Dimensions.get('window').width - 16}
+        height={220}
+        chartConfig={{
+          backgroundColor: '#1cc910',
+          backgroundGradientFrom: '#eff3ff',
+          backgroundGradientTo: '#efefef',
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        accessor="population"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        absolute
+      />
     </View>
   );
 };
